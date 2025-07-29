69b04e6a71268888d683ceb3a007952b4221f657
Mario-Mihai Mateas <mateasmario@ibm.com>
2025-07-29 13:34:21 +0300

    back

--- frontend/src/components/auth/Account.tsx
+++ frontend/src/components/auth/Account.tsx
@@ -1,16 +1,58 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useAuth} from "../../context/AuthenticationContext.tsx";
 import {useNavigate} from "react-router-dom";
 import {useTheme} from "../../context/ThemeContext.tsx";
 
+interface ProfileType {
+    isAdmin: boolean;
+    biography: string;
+    birthYear: string;
+}
+
 function Account() {
-    const {auth} = useAuth();
+    const {auth, initAuth} = useAuth();
     const navigate = useNavigate();
     const {theme} = useTheme();
+    const [profile, setProfile] = useState<ProfileType | null>({biography: "", birthYear: "", isAdmin: false});
+
+    const [editBiography, setEditBiography] = useState(false);
+    const [editBirthYear, setEditBirthYear] = useState(false);
+
+    const handleLogoutPress = () => {
+        initAuth("", "", "");
+        navigate("/");
+    }
+
+    const handleEditBiographyPress = () => {
+        setEditBirthYear(false);
+        setEditBiography(true);
+    }
+
+    const handleEditBirthYearPress = () => {
+        setEditBiography(false);
+        setEditBirthYear(true);
+    }
+
+    const handleCancelEditBiographyPress = () => {
+        setEditBiography(false);
+    }
+
+    const handleCancelEditBirthYearPress = () => {
+        setEditBirthYear(false);
+    }
 
     useEffect(() => {
         if (auth.token == "") {
-            navigate("/login");
+            navigate("/signin");
+        } else {
+            fetch("https://bwxor.com/api/profile/" + auth.email)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    setProfile(data);
+                })
+                .catch((error) => console.error(error));
         }
     }, [])
 
@@ -18,7 +60,14 @@ function Account() {
         <>
             <div className="account-group">
                 <div className="account-group-item">
-                    <h1>{auth.displayName}</h1>
+                    <div className="account-group-header">
+                        <div className="account-group-header-title">
+                            {auth.displayName}
+                        </div>
+                        <div className="account-group-header-logout">
+                            <a href="#" onClick={handleLogoutPress}>Logout</a>
+                        </div>
+                    </div>
                 </div>
                 <div className="account-group-item">
                     <div className="account-group-label">
@@ -33,23 +82,59 @@ function Account() {
                         <span className="fa-solid fa-ranking-star"></span> <strong>Rank</strong>
                     </div>
                     <div className={"account-group-content account-group-content-" + theme}>
-                        Member
+                        {profile != null && profile.isAdmin ?
+                            "Admin" : "Member"
+                        }
                     </div>
                 </div>
                 <div className="account-group-item">
                     <div className="account-group-label">
-                        <span className="fa-solid fa-info-circle"></span> <strong>Biography</strong> <a href="#">Edit</a>
+                        <span className="fa-solid fa-info-circle"></span> <strong>Biography</strong>
+                        {editBiography ?
+                            <>
+                                <a href="#" onClick={handleCancelEditBiographyPress}>Cancel</a>
+                                <a href="#" onClick={handleEditBiographyPress}>Save</a>
+                            </> :
+                            <>
+                                <a href="#" onClick={handleEditBiographyPress}>Edit</a>
+                            </>
+                        }
                     </div>
                     <div className={"account-group-content account-group-content-" + theme}>
-                        Write something about yourself...
+                        {editBiography ?
+                            <>
+                                <textarea placeholder="Write something interesting here..."
+                                          className={"textarea textarea-" + theme}>{profile?.biography}</textarea>
+                            </>
+                            :
+                            profile?.biography
+                        }
                     </div>
                 </div>
                 <div className="account-group-item">
                     <div className="account-group-label">
-                        <span className="fa-solid fa-calendar"></span> <strong>Birth year</strong> <a href="#">Edit</a>
+                        <span className="fa-solid fa-calendar"></span> <strong>Birth year</strong>
+                        {
+                            editBirthYear ?
+                                <>
+                                    <a href="#" onClick={handleCancelEditBirthYearPress}>Edit</a>
+                                    <a href="#" onClick={handleEditBirthYearPress}>Edit</a>
+                                </> :
+                                <>
+                                    <a href="#" onClick={handleEditBirthYearPress}>Edit</a>
+                                </>
+                        }
                     </div>
                     <div className={"account-group-content account-group-content-" + theme}>
-                        Hidden
+                        {
+                            editBirthYear ?
+                                <input type="number" className={"textbox textbox-" + theme}
+                                       min="1900"
+                                       max="2025"
+                                       defaultValue={profile?.birthYear}/>
+                                :
+                                profile?.birthYear
+                        }
                     </div>
                 </div>
             </div>
