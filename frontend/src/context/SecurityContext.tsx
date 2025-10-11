bd705cb4b87677135b223e2e9de18fdf2542de1b
mateasmario <mmmateas@gmail.com>
2025-10-11 11:24:15 +0300

    refix

--- frontend/src/context/SecurityContext.tsx
+++ frontend/src/context/SecurityContext.tsx
@@ -14,14 +14,14 @@ const SecurityContext = createContext<SecurityContextType | undefined>(undefined
 export const SecurityContextProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
     const getInitialSecurity = (): SecurityType => {
         return {
-            invalidLoginCount: sessionStorage.getItem("invalidLoginCount") || 0,
+            invalidLoginCount: parseInt(sessionStorage.getItem("invalidLoginCount") | null) || 0,
         };
     };
 
     const [security, setSecurity] = useState<SecurityType>(getInitialSecurity);
 
     useEffect(() => {
-        const invalidLoginCount = parseInt(sessionStorage.getItem("invalidLoginCount"));
+        const invalidLoginCount = parseInt(sessionStorage.getItem("invalidLoginCount") | null);
         if (invalidLoginCount != undefined) {
             setSecurity({
                 invalidLoginCount,
@@ -34,7 +34,7 @@ export const SecurityContextProvider: React.FC<{children: React.ReactNode}> = ({
             invalidLoginCount,
         });
 
-        sessionStorage.setItem("invalidLoginCount", string(invalidLoginCount));
+        sessionStorage.setItem("invalidLoginCount", invalidLoginCount.toString());
     }
 
     return (
